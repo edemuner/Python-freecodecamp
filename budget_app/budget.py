fe43fb07101850d4e0f2cb5a8289201db03e0380
edemuner <eduardood15@hotmail.com>
2021-10-03 15:25:12 -0300

    construção da classe Category, e os métodos mais simples

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -1,2 +1,22 @@
 class Category:
 
+    name = ''
+    ledger = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def deposit(self, amount, description=''):
+            self.ledger.append( {"amount": amount, "description":description} )
+
+    def withdraw(self, amount, description=''):
+        self.ledger.append( {"amount": - amount, "description": description} )
+
+    def get_balance(self):
+        balance = 0
+        for item in self.ledger:
+            balance += item["amount"]
+        return balance
+
+
+
