d4d28c249945c3f602ab0c27258eccea889384c1
Eduardo Demuner <82410623+edemuner@users.noreply.github.com>
2021-10-03 12:16:53 -0300

    adicionando a descrição do projeto

--- arithmetic_formatter/arithmetic_formatter/arithmetic_arranger.py
+++ arithmetic_formatter/arithmetic_formatter/arithmetic_arranger.py
@@ -1,3 +1,45 @@
+"""
+Students in primary school often arrange arithmetic problems vertically to make them easier to solve. For example, "235 + 52" becomes:
+```
+  235
++  52
+-----
+```
+
+Create a function that receives a list of strings that are arithmetic problems and returns the problems arranged vertically and side-by-side. The function should optionally take a second argument. When the second argument is set to `True`, the answers should be displayed.
+
+### For example
+
+Function Call:
+```py
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+```
+
+Output:
+```
+   32      3801      45      123
++ 698    -    2    + 43    +  49
+-----    ------    ----    -----
+```
+
+Function Call:
+```py
+arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+```
+
+Output:
+```
+  32         1      9999      523
++  8    - 3801    + 9999    -  49
+----    ------    ------    -----
+  40     -3800     19998      474
+  
+ 
+ RULES: https://replit.com/github/freeCodeCamp/boilerplate-arithmetic-formatter
+ 
+ """
+
+
 def arithmetic_arranger(operations, answer=False):
     if len(operations) > 5:
         return 'Error: Too many problems.'
