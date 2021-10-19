34d9523f3a1e60fcecf615a6f2fd79e0a64a9274
edemuner <eduardood15@hotmail.com>
2021-10-19 17:05:49 -0300

    preparada a classe rectangle, com os getters e setters

---
+++ polygon_area_calculator/shape_calculator.py
@@ -0,0 +1,22 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_widt(self, new_width):
+        self.width = new_width
+
+    def set_height(self, new_height):
+        self.height = new_height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self. height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+
