a5173693b3bea7303b0246028efb7e186b7f0d75
edemuner <eduardood15@hotmail.com>
2021-10-21 17:55:57 -0300

    aperfeiçoado o init e o atributo contents, para ficar de acordo com o assignment

--- probability_calculator/main.py
+++ probability_calculator/main.py
@@ -2,4 +2,4 @@ from prob_calculator import Hat
 
 hat = Hat('blue=2', 'red=4')
 
-print(hat.colors_and_numbers)
+print(hat.contents)
--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -1,8 +1,9 @@
 class Hat:
 
-    colors_and_numbers = {}
+    contents = []
 
     def __init__(self, *args):
         for i in args:
             color, number = i.split('=')
-            self.colors_and_numbers[color] = number
+            for j in range(int(number)):
+                self.contents.append(color)
