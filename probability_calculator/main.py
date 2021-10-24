ae6bdfb620aadbeed95fc1f964e1e7d744fc6fef
edemuner <eduardood15@hotmail.com>
2021-10-24 19:20:10 -0300

    terminada a função experiments, iniciando os testes

--- probability_calculator/main.py
+++ probability_calculator/main.py
@@ -1,6 +1,13 @@
-from prob_calculator import Hat
+from prob_calculator import Hat, experiment
 
-hat = Hat('blue=2', 'red=4')
+hat = Hat('blue=2', 'red=4', 'yellow=6')
 
 print(hat.contents)
-print(hat.draw(2))
+print(hat.draw(6))
+
+
+teste = experiment(hat,
+           {'red': 2, 'yellow': 3, 'blue': 0},
+           10,
+           1000)
+print(teste)
--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -22,4 +22,12 @@ def experiment(hat,
                expected_balls,
                num_balls_drawn,
                num_experiments):
-    pass
+    times = 0
+
+    for exp in num_experiments:
+
+        drawn = hat.draw(num_balls_drawn)
+        for ball, number in expected_balls.items():
+            if drawn.count(ball) == number:
+                times += 1
+    return times / num_experiments
